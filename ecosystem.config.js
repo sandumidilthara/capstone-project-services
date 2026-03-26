d84b9092547537a4e446fe48708150988a77c419
sandumi <sandumidilthara729@example.com>
2026-03-26 20:51:56 +0530

    f

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   apps : [
+    {
+     name : "cloud-sql-auth-proxy" ,
+     script: " ./cloud-sql-proxy capstone-491116:asia-southeast1:mysql-vm   capstone-491116:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy-log"
+    },
       {
     name   : "student-service",
     script : "java -jar student-service/target/Student-Service-1.0.0.jar",
