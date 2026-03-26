e6fb180d724d1583595064bdea2968c94012ee34
sandumi <sandumidilthara729@example.com>
2026-03-26 17:54:23 +0530

    f

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,18 @@ module.exports = {
   apps : [
       {
     name   : "student-service",
-    script : "./app.js"
+    script : "java -jar student-service/target/Student-Service-1.0.0.jar",
+    log_file: "./logs/student-service.log"
     },
     {
       name   : "program-service",
-      script : "./app.js"
+      script : "java -jar program-service/target/Program-Service-1.0.0.jar",
+      log_file: "./logs/program-service.log"
     },
     {
       name   : "enrollment-service",
-      script : "./app.js"
+      script : "java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
+        log_file: "./logs/enrollment-service.log"
     },
 
   ]
