cb7ffb227aebad18283431178baf074b3a1d6d46
sandumi <sandumidilthara729@example.com>
2026-03-26 17:07:17 +0530

    f

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [
+      {
+    name   : "student-service",
+    script : "./app.js"
+    },
+    {
+      name   : "program-service",
+      script : "./app.js"
+    },
+    {
+      name   : "enrollment-service",
+      script : "./app.js"
+    },
+
+  ]
+}
