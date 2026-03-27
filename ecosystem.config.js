bf27ef02a0591692f7a0682d603666442535ec82
sandumi <sandumidilthara729@example.com>
2026-03-27 12:54:08 +0530

    f

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,34 @@
 module.exports = {
-  apps : [
+  apps: [
     {
-     name : "cloud-sql-auth-proxy" ,
-     script: " ./cloud-sql-proxy capstone-491116:asia-southeast1:mysql-vm   capstone-491116:asia-southeast1:postgres-vm --private-ip",
-      log_file: "./logs/cloud-sql-proxy-log"
-    },
-      {
-    name   : "student-service",
-    script : "java -jar student-service/target/Student-Service-1.0.0.jar",
-    log_file: "./logs/student-service.log",
-     instances: 2,
+      name: "cloud-sql-auth-proxy",
+      script: "./cloud-sql-proxy",
+      args: "capstone-491116:asia-southeast1:mysql-vm capstone-491116:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy-log",
     },
     {
-      name   : "program-service",
-      script : "java -jar program-service/target/Program-Service-1.0.0.jar",
+      name: "student-service",
+      script: "java",
+      args: "-jar student-service/target/Student-Service-1.0.0.jar",
+      cwd: "/home/bestwayconsultancy2025/capstone-project-services",
+      log_file: "./logs/student-service.log",
+      instances: 2,
+    },
+    {
+      name: "program-service",
+      script: "java",
+      args: "-jar program-service/target/Program-Service-1.0.0.jar",
+      cwd: "/home/bestwayconsultancy2025/capstone-project-services",
       log_file: "./logs/program-service.log",
       instances: 2,
     },
     {
-      name   : "enrollment-service",
-      script : "java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
+      name: "enrollment-service",
+      script: "java",
+      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
+      cwd: "/home/bestwayconsultancy2025/capstone-project-services",
       log_file: "./logs/enrollment-service.log",
       instances: 2,
     },
-
   ]
 }
